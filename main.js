7efd433bb54644ded30987d4d04ac1aa52f50605
Alloy22 <aloysius22@hotmail.sg>
2021-09-22 19:26:08 +0800

    AbortController

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ const bot = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord
 bot.commands = new Discord.Collection();
 bot.events = new Discord.Collection();
 
+global.AbortController = require("node-abort-controller").AbortController;
+
 ["command_handler", "event_handler"].forEach(handler =>{
     require(`./handlers/${handler}`)(bot, Discord);
 })
 
-// bot.on('ready', () => {
-//     console.log("Bot is online")
-//     bot.user.setActivity("testing", {type: 'COMPETING', name: "abc"})
-// })
-
 bot.login(process.env.token);//process.env.token
+
+
+// "engines": {
+//   "node": "16.x"
+// }
