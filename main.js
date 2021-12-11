7074f2d4f58564fd1bf3cbae67ee17f9a7197534
Alloy22 <aloysius22@hotmail.sg>
2021-12-11 18:24:30 +0800

    patch

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const Discord= require('discord.js');
+const Discord = require('discord.js');
 const bot = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_VOICE_STATES] }); //{ partials: ["MESSAGE", "CHANNEL", "REACTION"] }
 bot.commands = new Discord.Collection();
 bot.events = new Discord.Collection();
 
-["command_handler", "event_handler"].forEach(handler =>{
+["command_handler", "event_handler"].forEach(handler => {
     require(`./handlers/${handler}`)(bot, Discord);
 })
 
@@ -12,4 +12,4 @@ bot.events = new Discord.Collection();
 //     bot.user.setActivity("testing", {type: 'COMPETING', name: "abc"})
 // })
 
-bot.login(process.env.token);//process.env.token
+bot.login(process.env.token); //process.env.token
